7b9d94fbee94062ac1e53ed32e3076e930d156f4
Prathibha Ratnayake <prsthibha@gmail.com>
2023-02-04 10:10:51 +0530

    complete product preview

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ProductPreview from 'src/components/ProductPreview/ProductPreview';
-import styles from './app.module.css'
 
 export function App() {
-  return (
-    <>
-      <h1 className={styles.heading}>Frontend Challenges</h1>
-      <ProductPreview/>
-      <div />
-    </>
-  );
+  return <ProductPreview />;
 }
 
 export default App;
--- src/components/ProductPreview/ProductPreview.tsx
+++ src/components/ProductPreview/ProductPreview.tsx
@@ -1,9 +1,49 @@
-
+import cart from '../../assets/icon-cart.svg';
+import productDesktop from '../../assets/image-product-desktop.jpg';
+import productMobile from '../../assets/image-product-mobile.jpg';
+import styles from './ProductPreview.module.css';
+import {useEffect, useState} from 'react'
 
 const ProductPreview = () => {
-  return (
-    <div>ProductPreview</div>
-  )
-}
 
-export default ProductPreview
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  return (
+    <div className={styles.card}>
+      <div className={styles.left}>
+        <img
+          className={styles.productImage}
+          src={screenWidth <= 600 ? productMobile : productDesktop}
+          alt=""
+        />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.rightContent}>
+          <p>P E R F U ME</p>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p className={styles.description}>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+          <div className={styles.priceContainer}>
+            <span className={styles.price}>$149.99</span>
+            <span className={styles.price2}>$169.99</span>
+          </div>
+          <div role="button" className={styles.cart}>
+            <img className="cartIcon" src={cart} alt="" />
+            Add to Cart
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPreview;
