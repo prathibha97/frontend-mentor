8dd52b2cdcd3004e5df67d911dcc8e5c4edded05
Prathibha Ratnayake <prsthibha@gmail.com>
2023-02-04 11:02:23 +0530

    Initial commit

---
+++ interactive-rating/src/app/App.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Rating from '../components/Rating';
+
+
+export function App() {
+  return (
+    <>
+      <Rating/>
+      <div/>
+    </>
+  );
+}
+
+export default App;
---
+++ interactive-rating/src/components/Rating/index.tsx
@@ -0,0 +1,9 @@
+import './Rating.module.css'
+
+const Rating = () => {
+  return (
+    <div>Rating</div>
+  )
+}
+
+export default Rating
